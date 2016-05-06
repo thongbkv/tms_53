41c60b98ddba1216b660e05cf9b19b43aec30f35
thongbkv <framgia@framgia0160-lt.framgia.com>
2016-05-06 15:36:14 +0700

    View-all-Trainees

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get "login" => "sessions#new"
   post "login" => "sessions#create"
   delete "logout" => "sessions#destroy"
-  get "signup" => "users#new"
-  resources :users, only: [:new, :create]
+  resources :users, except: [:edit, :update, :destroy]
 end

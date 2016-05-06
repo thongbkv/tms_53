6a754d75ff1b5934d52dbe51dcad6448c4160a51
thongbkv <framgia@framgia0160-lt.framgia.com>
2016-05-06 15:36:14 +0700

    sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,20 @@ class UsersController < ApplicationController
   def show
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "create.message_success"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password,

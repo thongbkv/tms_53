c7284a6f19486c10b8e45a3b3458ccea9f58c48c
thongbkv <framgia@framgia0160-lt.framgia.com>
2016-05-06 15:36:14 +0700

    sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   attr_accessor :remember_token
   validates :name,  presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save {self.email == email.downcase}
   validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: true

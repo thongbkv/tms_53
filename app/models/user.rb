22b54c143c2694680c2ff041a789104b3942d71b
thongbkv <framgia@framgia0160-lt.framgia.com>
2016-05-11 11:14:43 +0700

    following-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,4 +50,16 @@ class User < ActiveRecord::Base
   def forget
     update_attributes remember_digest: nil
   end
+
+  def follow other_user
+    active_relationships.create followed_id: other_user.id
+  end
+
+  def unfollow other_user
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following? other_user
+    following.include? other_user
+  end
 end
